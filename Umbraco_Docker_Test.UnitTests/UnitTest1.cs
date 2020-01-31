2c998a2eb7b267b6bda801d529e9cc9d0153877b
Tom Doyle <tom.doyle.im@gmail.com>
2020-01-31 11:48:51 +0000

    Add project files.

---
+++ Umbraco_Docker_Test.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Umbraco_Docker_Test.UnitTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
